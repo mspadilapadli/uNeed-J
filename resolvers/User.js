e7bfdfaca657717bab570bc9ed38fb9024e88be2
mspadilapadli <msokhibulpadilapadli@gmail.com>
2024-05-02 17:02:22 +0700

    feat: login & get access_token

--- resolvers/User.js
+++ resolvers/User.js
@@ -1,7 +1,14 @@
+const { GraphQLError } = require("graphql");
 const User = require("../models/User");
+const { comparePassword } = require("../helpers/bcrypt");
+const { createToken } = require("../helpers/jwt");
 
 const resolvers = {
     Query: {
+        getUsers: async () => {
+            return await User.getUsers();
+        },
+
         getUserById: async (_, args) => {
             const { _id } = args;
             return await User.findUserById(_id);
@@ -23,7 +30,42 @@ const resolvers = {
             console.log(result);
             return result;
         },
-        // login: async () => {},
+        login: async (_, args) => {
+            const { email, password } = args;
+            if (!email || !password) {
+                throw new GraphQLError("Invalid Input ", {
+                    extensions: {
+                        code: "Bad Request",
+                    },
+                });
+            }
+
+            const user = await User.findUserByEmail(email);
+            if (!user) {
+                throw new GraphQLError("Invalid Email ", {
+                    extensions: {
+                        code: "UNAUTHORIZED",
+                    },
+                });
+            }
+
+            const comparePass = comparePassword(password, user.password);
+            if (!comparePass) {
+                throw new GraphQLError("Invalid email or password ", {
+                    extensions: {
+                        code: "UNAUTHORIZED",
+                    },
+                });
+            }
+
+            const access_token = createToken({
+                _id: user._id,
+                email: user.email,
+            });
+            console.log(access_token, "accesstoken");
+
+            return { access_token };
+        },
     },
 };
 
