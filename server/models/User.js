31622b22d8031bd3f23384230c9f8c1f42e98f34
mspadilapadli <msokhibulpadilapadli@gmail.com>
2024-05-03 15:51:23 +0700

    setup: move to folder server

---
+++ server/models/User.js
@@ -0,0 +1,39 @@
+const { ObjectId } = require("mongodb");
+const { database } = require("../config/mongodb");
+const { hashPassword } = require("../helpers/bcrypt");
+
+class User {
+    static collection() {
+        return database.collection("User");
+    }
+
+    static async getUsers() {
+        return this.collection().find().toArray();
+    }
+
+    static async findUserById(_id) {
+        return await this.collection().findOne({
+            _id: new ObjectId(String(_id)),
+        });
+        // return this.collection().findOne({ _id: new ObjectId(`${_id}`) });
+    }
+
+    static async addUser({ name, username, email, password }) {
+        // console.log(newUser, "newUser model");
+
+        return this.collection().insertOne({
+            name,
+            username,
+            email,
+            password: hashPassword(password),
+        });
+    }
+
+    static async findUserByEmail(email) {
+        return await this.collection().findOne({
+            email,
+        });
+        // return this.collection().findOne({ _id: new ObjectId(`${_id}`) });
+    }
+}
+module.exports = User;
---
+++ server/schema/Posts.js
@@ -0,0 +1,48 @@
+const typeDefs = `#graphql
+
+type Post {
+   _id: ID
+   content: String
+   tags: [String]
+   imgUrl: String 
+   authorId: String 
+   comments: [Comments]
+   likes: [Likes]
+   createdAt: String
+   updatedAt: String
+}
+
+type Comments {
+   content: String
+   username: String
+   createdAt: String
+   updatedAt: String
+}
+
+type Likes {
+   username: String
+   createdAt: String
+   updatedAt: String
+}
+
+type Query {
+   getPosts:[Post]
+   getPostById(_id:ID!): Post
+}
+
+input InputDataPost {
+   content: String
+   tags: [String]
+   imgUrl: String 
+}
+
+
+type Mutation {
+   createPost(newPost: InputDataPost):Post
+   createComment(_id: ID!, content: String): Post
+   createLike(_id: ID!): Post
+}
+
+`;
+
+module.exports = typeDefs;
