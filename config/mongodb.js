aa22b41a238547985ce6eced314c0da29b08267a
mspadilapadli <msokhibulpadilapadli@gmail.com>
2024-05-03 15:52:40 +0700

    setup: move to folder server

--- config/mongodb.js
+++
@@ -1,9 +0,0 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri =
-    "mongodb+srv://mspadilapadlidev:zoPeMOS5XZkgn9ZE@cluster0.nhsjhxg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// Create a new client and connect to MongoDB
-const client = new MongoClient(uri);
-const database = client.db("uNeed-J");
-
-module.exports = { client, database };
--- helpers/bcrypt.js
+++
@@ -1,7 +0,0 @@
-const { hashSync, compareSync } = require("bcryptjs");
-
-module.exports = {
-    hashPassword: (password) => hashSync(password),
-    comparePassword: (passwordInput, passwordDB) =>
-        compareSync(passwordInput, passwordDB),
-};
